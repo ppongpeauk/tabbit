f7e47c7d21e6466109cafd4ffc701dca0df82cd1
Pete Pongpeauk <kurtsiberg@gmail.com>
2025-12-04 18:01:19 -0500

    fix: railway deployment

--- server/src/lib/resend.ts
+++ server/src/lib/resend.ts
@@ -8,3 +8,4 @@ import { env } from "@/config/env";
 
 export const resend = new Resend(env.RESEND_API_KEY);
 
+
--- server/src/modules/auth/service.ts
+++ server/src/modules/auth/service.ts
@@ -287,3 +287,4 @@ export class AuthService {
 }
 
 export const authService = new AuthService();
+
